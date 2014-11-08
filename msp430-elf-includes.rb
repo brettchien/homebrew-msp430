a56bb9f23cff873be161853fd9e6d2be6a8b4329
Bradford Barr <bradford.barr@gmail.com>
2014-11-08 11:34:55 -0800

    Adding sha1 hash.

--- msp430-elf-includes.rb
+++ msp430-elf-includes.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Msp430ElfIncludes < Formula
   homepage 'http://www.ti.com/tool/msp430-gcc-opensource'
   url 'http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/latest/exports/msp430-support-files.zip'
+  sha1 '791f99fa2506a7b85572401131ff9416d1acdbc8'
 
   def install
     onoe 'This package is installed by msp430-elf-gcc.'
