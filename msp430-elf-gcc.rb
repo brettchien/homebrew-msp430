30aeb5fe943dc1b403a84d920f2550fd2975f90b
Bradford Barr <bradford.barr@gmail.com>
2014-11-08 00:43:13 -0800

    Check to see if this happens on 4.9.1.

--- msp430-elf-gcc.rb
+++ msp430-elf-gcc.rb
@@ -2,9 +2,14 @@ require 'formula'
 
 class Msp430ElfGcc < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftpmirror.gnu.org/gcc/gcc-4.9.2/gcc-4.9.2.tar.bz2'
-  mirror 'ftp://gcc.gnu.org/pub/gcc/releases/gcc-4.9.2/gcc-4.9.2.tar.bz2'
-  sha1 '79dbcb09f44232822460d80b033c962c0237c6d8'
+
+  # url 'http://ftpmirror.gnu.org/gcc/gcc-4.9.2/gcc-4.9.2.tar.bz2'
+  # mirror 'ftp://gcc.gnu.org/pub/gcc/releases/gcc-4.9.2/gcc-4.9.2.tar.bz2'
+  # sha1 '79dbcb09f44232822460d80b033c962c0237c6d8'
+
+  url 'http://ftpmirror.gnu.org/gcc/gcc-4.8.2/gcc-4.8.2.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.8.2/gcc-4.8.2.tar.bz2'
+  sha1 '810fb70bd721e1d9f446b6503afe0a9088b62986'
 
   head 'svn://gcc.gnu.org/svn/gcc/branches/gcc-4_9-branch'
 
@@ -52,8 +57,7 @@ class Msp430ElfGcc < Formula
     mkdir 'build' do
       system '../configure', *args
       system 'make', 'all'
-      system 'make', 'install-host'
-      system 'make', 'install-target'
+      system 'make', 'install'
     end
 
     info.rmtree
