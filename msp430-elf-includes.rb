fa26ecf80066bc0779c07700e8bc127a1c5dd0db
brettchien <brettchien@users.noreply.github.com>
2014-11-30 18:44:24 -0800

    update downloaded file SHA1 checksum

--- msp430-elf-includes.rb
+++ msp430-elf-includes.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Msp430ElfIncludes < Formula
   homepage 'http://www.ti.com/tool/msp430-gcc-opensource'
   url 'http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/latest/exports/msp430-gcc-support-files.zip'
-  sha1 '791f99fa2506a7b85572401131ff9416d1acdbc8'
+  sha1 '0fad3ca29236149034edcc75d9618c3aee8e653b'
 
   depends_on 'msp430-elf-gcc'
 
